79b59396bff6342f2266af058f4996b4a49b2ed6
Jan Joshua De Guzman <janjoshuadeguzman00@gmail.com>
2025-10-25 18:18:09 +0000

    feat: Created a http server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
 import e from "express";
+import http from "node:http";
 
 const app = e();
+const port = 8080;
+const host = "127.0.0.1";
 
 app.use(e.json());
 
 app.use(e.urlencoded({ extended: false }));
+
+app.get("/", (_, res) => {
+  res.status(200).json({ message: "Hello, World!" });
+});
+
+const server = http.createServer(app);
+
+server.on("error", (error) => {
+  const { message } = error;
+
+  console.error(`[ERROR]: ${message}`);
+});
+
+server.on("request", (req) => {
+  const { url, method } = req;
+
+  console.log(`[INFO]: ${method} ${url}`);
+});
+
+server.on("listening", () => {
+  console.log(`Server running http://${host}:${port}`);
+});
+
+server.listen({ port, host });
